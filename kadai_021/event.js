0c506184efa2dda9ebdefd1c1748faa9c49a0214
katsumi0501 <hono2ko2ka@icloud.com>
2025-07-03 15:37:04 +0900

    Delete kadai_021 directory

--- kadai_021/event.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('btn');
-const textElemant = document.getElementById('text');
-
-btn.addEventListener('click', () => {
-    setTimeout(() => {
-        textElemant.textContent = 'ボタンをクリックしました';
-    }, 2000);
-});
